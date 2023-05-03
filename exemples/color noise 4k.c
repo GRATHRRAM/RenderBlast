e64b7d33fd8611e2c815518168eadbf5d1037dc8
Script_guy <101575465+scriptguy1@users.noreply.github.com>
2023-05-03 10:14:36 +0200

    Add files via upload

---
+++ exemples/color noise 4k.c
@@ -0,0 +1,41 @@
+#include "RBlast.c"
+#include "RBlastConv.c"
+
+//4k color noise!
+#define resx 3840
+#define resy 2140
+#define max resx * resy
+
+int main(void)
+{
+    RB_RENDER rnd;
+    RB_NEW_RENDER(&rnd,resx,resy);
+
+    RB_COLOR clr;
+
+    int counter = 0,cout2 = 0;
+    for (int y = 0; y < rnd.RESOLUTION_Y; y++)
+    {
+        for (int x = 0; x < rnd.RESOLUTION_X; x++)
+        {
+            clr[0] = rand() % 255;
+            clr[1] = rand() % 255;
+            clr[2] = rand() % 255;
+            RB_RENDER_SET_PIXEL(&rnd,x,y,clr);
+            counter++;
+            if(counter > 550)
+            {
+                printf("progres -> %i / %i\n",cout2,max);
+                cout2 += counter;
+                counter = 0;
+            }
+        }
+    }
+    
+    RBC_RENDER2PPM(&rnd,"yes1.ppm");
+
+    printf("done!\n");
+    getchar();getchar();
+
+    return 0;
+}
